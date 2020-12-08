01859d60b79e56f5cc2233cd923172355146926d
Alexander Römer <alex@roemer.design>
2020-12-08 15:09:32 +0100

    Initial Commit

---
+++ ARF/ARFApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARFApp.swift
+//  ARF
+//
+//  Created by Alexander Römer on 08.12.20.
+//
+
+import SwiftUI
+
+@main
+struct ARFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ARF/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ARF
+//
+//  Created by Alexander Römer on 08.12.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
